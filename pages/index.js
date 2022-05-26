f0e646bd1d736d4a113baf5b4359487abef745c7
Mohammad Quadri <quadrixm@gmail.com>
2022-05-26 09:07:33 +0530

    adding next js files

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function HomePage() {
+    return (
+        <div>
+            Hi I am Mohammad. If you would like to add your page on this web app.
+            Please add a file with your name. Say for example you are alice,
+            you will create a file alice.js in the pages folder and in it write "Hi I am Alice and your other information.
+            And after that send a merge request.
+        </div>
+    );
+  }
+  
+  export default HomePage
